4cf1b08596477db03452a1d59ea2c97abd9338fe
Henry Nguyen <Henry.nguyen@stonybrook.edu>
2021-02-16 17:16:43 -0500

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Henry Nguyen on 2/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
